bb2fa1fef57ce3214415fcb9a9628de5595070e1
DvidMiguel <dvidmiguel@gmail.com>
2016-11-05 18:08:00 -0600

    Modificación: sql - agregar paciente
    Cambie la forma de encontrr el último id insertado, ya hay una función
    predeterminada en php para hacer eso

--- sql/agregar_paciente.php
+++ sql/agregar_paciente.php
@@ -13,17 +13,12 @@ $numero_documento= $_POST['numero_documento'];
 	$sql= "INSERT INTO datos_generales (nombre,apellido,fecha_nacimiento,genero) VALUES('$nombre','$apellido','$fecha','$genero')";
 	$resultado = $conexion -> query($sql);
 
-	$buscarID= "SELECT id_datos_generales FROM datos_generales WHERE nombre='$nombre' and apellido= '$apellido'";
-	$resultado = $conexion -> query($buscarID);
-
-	while( $row = $resultado -> fetch_assoc()){
-		   $id_datos_generales = $row['id_datos_generales'];
-		}
-	echo "<br>".$id_datos_generales;
+	$id_datos_generales = mysqli_insert_id($conexion);
            
 	$sql="INSERT INTO paciente (id_datos_generales,alergias,documento,responsable,numero_documento) VALUES ('$id_datos_generales','$alergias','$documento','$responsable','$numero_documento')";
 	$resultado = $conexion -> query($sql);
-            header("location: ../agregar_paciente.php");
+            
+    header("location: ../agregar_paciente.php");
 
  ?>
 
